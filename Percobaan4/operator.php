825f5df7033805f2a4c1ac0a44699762ed50ca83
Nandaricco04 <nanda.ricco777@gmail.com>
2025-09-21 20:44:47 +0700

    Praktikum 3

--- Percobaan4/operator.php
+++ Percobaan4/operator.php
@@ -9,18 +9,6 @@ $hasilBagi = $a / $b;
 $sisaBagi = $a % $b;
 $pangkat = $a ** $b;
 
-$hasilSama = $a == $b;
-$hasilTidakSama = $a != $b;
-$hasilLebihKecil = $a < $b;
-$hasilLebihBesar = $a > $b;
-$hasilLebihKecilSama = $a <= $b;
-$hasilLebihBesarSama = $a >= $b;
-
-$hasilAnd = $a && $b;
-$hasilOr = $a || $b;
-$hasilNotA = !$a;
-$hasilNotB = !$b;
-
 echo "Hasil Penjumlahan: $a + $b = $hasilTambah <br>";
 echo "Hasil Pengurangan: $a - $b = $hasilKurang <br>";
 echo "Hasil Perkalian: $a * $b = $hasilKali <br>";
@@ -28,6 +16,13 @@ echo "Hasil Pembagian: $a / $b = $hasilBagi <br>";
 echo "Sisa Hasil Bagi: $a % $b = $sisaBagi <br>";
 echo "Hasil Pangkat: $a ** $b = $pangkat <br>";
 echo "<br>";
+
+$hasilSama = $a == $b;
+$hasilTidakSama = $a != $b;
+$hasilLebihKecil = $a < $b;
+$hasilLebihBesar = $a > $b;
+$hasilLebihKecilSama = $a <= $b;
+$hasilLebihBesarSama = $a >= $b;
 
 echo "Hasil Sama: $a == $b : " . ($hasilSama ? 'true' : 'false') . "<br>";
 echo "Hasil Tidak Sama: $a != $b : " . ($hasilTidakSama ? 'true' : 'false') . "<br>";
@@ -37,8 +32,22 @@ echo "Hasil Lebih Kecil Sama: $a <= $b : " . ($hasilLebihKecilSama ? 'true' : 'f
 echo "Hasil Lebih Besar Sama: $a >= $b : " . ($hasilLebihBesarSama ? 'true' : 'false') . "<br>";
 echo "<br>";
 
+$hasilAnd = $a && $b;
+$hasilOr = $a || $b;
+$hasilNotA = !$a;
+$hasilNotB = !$b;
+
 echo "Hasil AND: $a && $b : " . ($hasilAnd ? 'true' : 'false') . "<br>";
 echo "Hasil OR: $a || $b : " . ($hasilOr ? 'true' : 'false') . "<br>";
 echo "Hasil NOT A: !$a : " . ($hasilNotA ? 'true' : 'false') . "<br>";
 echo "Hasil NOT B: !$b : " . ($hasilNotB ? 'true' : 'false') . "<br>";
+echo "<br>";
+
+$a += $b;
+$a -= $b;
+$a *= $b;
+$a /= $b;
+$a %= $b;
+
+echo "Nilai a: $a <br>";
 ?>
