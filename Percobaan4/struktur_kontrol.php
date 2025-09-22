0a94e2e2ef0e0b2492934af0c02010524f09d899
Nandaricco04 <nanda.ricco777@gmail.com>
2025-09-22 19:01:24 +0700

    Praktikum 4

--- Percobaan4/struktur_kontrol.php
+++ Percobaan4/struktur_kontrol.php
@@ -45,4 +45,17 @@ foreach ($skorUjian as $skor) {
 }
 echo "<br>";
 echo "Total skor ujian adalah: $totalSkor";
+echo "<br>";
+echo "<br>";
+
+$nilaiSiswa = [85, 92, 58, 64, 90, 55, 88, 79, 70, 96];
+
+foreach ($nilaiSiswa as $nilai) {
+    if ($nilai < 60) {
+        
+        echo "Nilai $nilai (Tidak Lulus) <br>";
+        continue;
+    }
+    echo "Nilai $nilai (Lulus) <br>";
+}
 ?>
