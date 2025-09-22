dac0f8c7a0efaa94d4c6b0f909e89fb48b6b5129
Nandaricco04 <nanda.ricco777@gmail.com>
2025-09-22 18:54:08 +0700

    Praktikum 4

--- Percobaan4/struktur_kontrol.php
+++ Percobaan4/struktur_kontrol.php
@@ -24,4 +24,15 @@ while ($jarakSaatIni < $jarakTarget) {
 echo "<br>";
 echo "Atlet tersebut memerlukan $hari hari untuk mencapai target jarak 500 km.";
 echo "<br>";
+
+$jumlahLahan = 10;
+$tanamanPerLahan = 5;
+$buahPerTanaman = 10;
+$jumlahBuah = 0;
+
+for ($i = 1; $i <= $jumlahLahan; $i++){
+    $jumlahBuah += ($tanamanPerLahan * $buahPerTanaman);
+}
+echo "<br>";
+echo "Jumlah buah yang akan dipanen adalah: $jumlahBuah";
 ?>
