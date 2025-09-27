fbeced239822f4f855ee2e1b875e6ebe56916e85
Nandaricco04 <nanda.ricco777@gmail.com>
2025-09-27 22:08:21 +0700

    Praktikum 11

--- JS05_PHP-2/global_post.php
+++ JS05_PHP-2/global_post.php
@@ -7,7 +7,6 @@
         </form>
         <?php
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            // collect value of input field
             $name = $_POST['fname'];
             if (empty($name)) {
                 echo "Name is empty";
