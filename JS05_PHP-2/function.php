427ae5150401bbb2c0ce1b81bf0b3c6b4d9dcf4b
Nandaricco04 <nanda.ricco777@gmail.com>
2025-09-27 21:08:10 +0700

    Praktikum 4

---
+++ JS05_PHP-2/function.php
@@ -0,0 +1,12 @@
+<?php
+
+function perkenalan(){
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan nama saya Nanda<br>";
+    echo "Senang berkenalan dengan anda<br>";
+}
+
+perkenalan();
+perkenalan();
+
+?>
