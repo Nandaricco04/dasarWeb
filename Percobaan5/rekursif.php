d8d436af5c32f584ca49b84be439b82c1a077efb
Nandaricco04 <nanda.ricco777@gmail.com>
2025-09-27 18:41:35 +0700

    rekursif

--- Percobaan5/rekursif.php
+++ Percobaan5/rekursif.php
@@ -1,9 +1,18 @@
 <?php
-function tampilkanHaloDunia(){
-    echo "Halo Dunia! <br>";
+// function tampilkanHaloDunia(){
+//     echo "Halo Dunia! <br>";
 
-    tampilkanHaloDunia();
+//     tampilkanHaloDunia();
+// }
+
+// tampilkanHaloDunia();
+
+function tampilkanAngka(int $jumlah, int $indexs = 1){
+    echo "Perulangan ke-{$indexs} <br>";
+
+    if ($indexs < $jumlah) {
+        tampilkanAngka($jumlah, $indexs + 1);
+    }
 }
-
-tampilkanHaloDunia();
+tampilkanAngka(20);
 ?>
