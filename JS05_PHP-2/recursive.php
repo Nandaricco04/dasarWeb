1752047890093200ea6c02ac30a8781346336fd9
Nandaricco04 <nanda.ricco777@gmail.com>
2025-09-27 21:20:52 +0700

    Praktikum 5

--- JS05_PHP-2/recursive.php
+++ JS05_PHP-2/recursive.php
@@ -5,4 +5,13 @@
 //     tampilkanHaloDunia();
 // }
 // tampilkanHaloDunia();
+
+function tampilkanAngka(int $jumlah, int $indexs = 1){
+    echo "Perulangan ke-{$indexs} <br>";
+
+    if ($indexs < $jumlah) {
+        tampilkanAngka($jumlah, $indexs + 1);
+    }
+}
+tampilkanAngka(20);
 ?>
