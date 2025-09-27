510542792d8a0fac67c2b185c021b4d69e1b8aec
Nandaricco04 <nanda.ricco777@gmail.com>
2025-09-27 18:53:18 +0700

    rekursif

--- Percobaan5/rekursif.php
+++ Percobaan5/rekursif.php
@@ -7,12 +7,60 @@
 
 // tampilkanHaloDunia();
 
-function tampilkanAngka(int $jumlah, int $indexs = 1){
-    echo "Perulangan ke-{$indexs} <br>";
+// function tampilkanAngka(int $jumlah, int $indexs = 1){
+//     echo "Perulangan ke-{$indexs} <br>";
 
-    if ($indexs < $jumlah) {
-        tampilkanAngka($jumlah, $indexs + 1);
+//     if ($indexs < $jumlah) {
+//         tampilkanAngka($jumlah, $indexs + 1);
+//     }
+// }
+// tampilkanAngka(20);
+
+$menu = [
+    [
+        "nama" => "Beranda",
+    ],
+    [
+        "nama" => "Berita",
+        "submenu" => [
+            [
+                "nama" => "Wisata",
+                "submenu" => [
+                    [
+                        "nama" => "Pantai"
+                    ],
+                    [
+                        "nama" => "Gunung"
+                    ]
+                ]
+            ],
+            [
+                "nama" => "Kuliner"
+            ],
+            [
+                "nama" => "Hiburan"
+            ]
+        ]
+    ],
+    [
+        "nama" => "Tentang"
+    ],
+    [
+        "nama" => "Kontak"
+    ]
+];
+
+function tampilkanMenuBertingkat(array $menu){
+    echo "<ul>";
+    foreach ($menu as $key => $item){
+        echo "<li>{$item["nama"]}</li>";
+        if (isset($item["submenu"])) {
+            tampilkanMenuBertingkat($item["submenu"]);
+        }
+        echo "</li>";
     }
+    echo "</ul>";
 }
-tampilkanAngka(20);
+
+tampilkanMenuBertingkat($menu);
 ?>
