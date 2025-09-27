637ec53c54b767ad85a44b506707ccb831292951
Nandaricco04 <nanda.ricco777@gmail.com>
2025-09-27 21:09:54 +0700

    Praktikum 4

--- JS05_PHP-2/function.php
+++ JS05_PHP-2/function.php
@@ -1,12 +1,26 @@
 <?php
 
-function perkenalan(){
-    echo "Assalamualaikum, ";
-    echo "Perkenalkan nama saya Nanda<br>";
+// soal 4
+// function perkenalan(){
+//     echo "Assalamualaikum, ";
+//     echo "Perkenalkan nama saya Nanda<br>";
+//     echo "Senang berkenalan dengan anda<br>";
+// }
+
+// perkenalan();
+// perkenalan();
+
+// soal 2 dan 3
+function perkenalan($nama, $salam="Assalamualaikum"){
+    echo "$salam, ";
+    echo "Perkenalkan, nama saya ".$nama."<br>";
     echo "Senang berkenalan dengan anda<br>";
 }
 
-perkenalan();
-perkenalan();
+perkenalan("Nanda", "Hallo");
 
+echo "<hr>";
+
+$saya = "Nanda";
+$ucapanSalam = "Selamat Pagi";
 ?>
