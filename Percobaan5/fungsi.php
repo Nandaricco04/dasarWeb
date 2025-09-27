a3c5a013f8cfb1d671fa8518e7642c4464e2c987
Nandaricco04 <nanda.ricco777@gmail.com>
2025-09-27 18:18:53 +0700

    fungsi

--- Percobaan5/fungsi.php
+++ Percobaan5/fungsi.php
@@ -1,11 +1,26 @@
 <?php
 
-function perkenalan(){
-    echo "Assalamualaikum, ";
-    echo "Perkenalkan nama saya Nanda<br>";
+// coba 1
+// function perkenalan(){
+//     echo "Assalamualaikum, ";
+//     echo "Perkenalkan nama saya Nanda<br>";
+//     echo "Senang berkenalan dengan anda<br>";
+// }
+
+// perkenalan();
+
+function perkenalan($nama, $salam){
+    echo "$salam, ";
+    echo "Perkenalkan, nama saya ".$nama."<br>";
     echo "Senang berkenalan dengan anda<br>";
 }
 
-perkenalan();
-perkenalan();
+perkenalan("Nanda", "Hallo");
+
+echo "<hr>";
+
+$saya = "Nanda";
+$ucapanSalam = "Selamat Pagi";
+
+perkenalan($saya, $ucapanSalam);
 ?>
