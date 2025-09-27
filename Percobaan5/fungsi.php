fec6c807e3fda5e98a6d9963d4b093d4c4ef06d7
Nandaricco04 <nanda.ricco777@gmail.com>
2025-09-27 18:25:27 +0700

    fungsi

--- Percobaan5/fungsi.php
+++ Percobaan5/fungsi.php
@@ -9,18 +9,26 @@
 
 // perkenalan();
 
-function perkenalan($nama, $salam="Assalamualaikum"){
-    echo "$salam, ";
-    echo "Perkenalkan, nama saya ".$nama."<br>";
-    echo "Senang berkenalan dengan anda<br>";
+// soal 2 dan 3
+// function perkenalan($nama, $salam="Assalamualaikum"){
+//     echo "$salam, ";
+//     echo "Perkenalkan, nama saya ".$nama."<br>";
+//     echo "Senang berkenalan dengan anda<br>";
+// }
+
+// perkenalan("Nanda", "Hallo");
+
+// echo "<hr>";
+
+// $saya = "Nanda";
+// $ucapanSalam = "Selamat Pagi";
+
+// perkenalan($saya);
+
+function hitungUmur($tahunLahir, $tahunSekarang){
+    $umur = $tahunSekarang - $tahunLahir;
+    return $umur;
 }
 
-perkenalan("Nanda", "Hallo");
-
-echo "<hr>";
-
-$saya = "Nanda";
-$ucapanSalam = "Selamat Pagi";
-
-perkenalan($saya);
+echo "Umur saya adalah ". hitungUmur(2005, 2025)." tahun";
 ?>
