216cc4a980a7b12442dc94cb346ab7c9d88d1b67
Nandaricco04 <nanda.ricco777@gmail.com>
2025-09-27 21:17:18 +0700

    Praktikum 4

--- JS05_PHP-2/function_return.php
+++ JS05_PHP-2/function_return.php
@@ -3,6 +3,12 @@ function hitungUmur($tahunLahir, $tahunSekarang){
     $umur = $tahunSekarang - $tahunLahir;
     return $umur;
 }
+function perkenalan($nama, $salam="Assalamualaikum"){
+    echo "$salam, ";
+    echo "Perkenalkan, nama saya ".$nama."<br>";
 
-echo "Umur saya adalah ".hitungUmur(2005, 2025)." tahun<br>";
+    echo "Saya berusia ".hitungUmur(2005, 2025)." tahun<br>";
+    echo "Senang berkenalan dengan anda<br>";
+}
+perkenalan("Nanda");
 ?>
