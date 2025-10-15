c2f46cfa22d96ff8f25fc0aeac67f36ca1d5aef7
Nandaricco04 <nanda.ricco777@gmail.com>
2025-10-15 19:18:23 +0700

    Praktikum5

--- Percobaan7/regex.php
+++ Percobaan7/regex.php
@@ -29,7 +29,25 @@ echo "<br>";
 $pattern = '/go*d/';
 $text = 'god is good.';
 if (preg_match($pattern, $text, $matches)) {
-    echo "Ditemukan: " . $matches[0];
+    echo "Cocokkan: " . $matches[0];
 } else {
-    echo "Tidak ditemukan yang cocok!";
+    echo "Tidak ada yang cocok!";
+}
+
+echo "<br>";
+$pattern = '/go?d/';
+$text = 'god is good.';
+if (preg_match($pattern, $text, $matches)) {
+    echo "Cocokkan: " . $matches[0];
+} else {
+    echo "Tidak ada yang cocok!";
+}
+
+echo "<br>";
+$pattern = '/go{1,2}d/';
+$text = 'god is good.';
+if (preg_match($pattern, $text, $matches)) {
+    echo "Cocokkan: " . $matches[0];
+} else {
+    echo "Tidak ada yang cocok!";
 }
